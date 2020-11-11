203cb47440f45e0ecf1680f3a9c32747c1af3dc9
iamcid <iamcid17@gmail.com>
2020-11-11 01:38:10 -0500

    wires up sign up functionality and adds user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     has_many :reviewed_baby_foods, through: :reviews, source: :baby_food
 
     has_many :baby_foods
+
+    validates :username, uniqueness: true, presence: true
+    validates :baby_name, presence: true
+
+    has_secure_password
 end
