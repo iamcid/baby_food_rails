7a339a490a523080c0a43ff46d42240d663007a8
iamcid <iamcid17@gmail.com>
2020-11-12 22:44:31 -0500

    adds and wires up index and show views

--- app/controllers/baby_foods_controller.rb
+++ app/controllers/baby_foods_controller.rb
@@ -2,7 +2,7 @@ class BabyFoodsController < ApplicationController
 
     def new
         @baby_food = BabyFood.new
-        @baby_food = build_category
+        @baby_food.build_category
     end
 
     def create
@@ -11,13 +11,22 @@ class BabyFoodsController < ApplicationController
         if @baby_food.save
             redirect_to baby_food_path(@baby_food)
         else
+            @baby_food.build_category
             render :new
         end
+    end
+
+    def index
+        @baby_foods = BabyFood.all
+    end
+
+    def show
+        @baby_food = BabyFood.find_by_id(params[:id])
     end
 
     private
 
     def baby_food_params
-        params.require(baby_food).permit(:name, :description, :category_id, category_attributes: [:name])
+        params.require(:baby_food).permit(:name, :description, :category_id, category_attributes: [:name])
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :baby_foods
+    validates :name, presence: true, uniqueness: true
 end
