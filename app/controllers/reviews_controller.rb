fd105ab681ca4dd1a038fb461cf2111b581dc24f
iamcid <iamcid17@gmail.com>
2020-11-18 02:59:23 -0500

    adds edit link to review shoe page and fixes review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -37,7 +37,7 @@ class ReviewsController < ApplicationController
 
     def update
         @review = Review.find(params[:id])
-        @review.update(thumbs_up: params[:review][:thumbs_up], content: params[:review][:content])
+        @review.update(review_params)
         redirect_to review_path(@review)
     end
 
